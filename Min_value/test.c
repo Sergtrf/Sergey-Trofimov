df814648cb5a9f71e4b3ad395c23bcaafa51d8bc
Sergtrf <sergey.trof@rambler.ru>
2021-04-19 00:02:21 +0300

    Метод измельчения равномерной сетки

--- Min_value/test.c
+++ Min_value/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
-#include "chord_method.h"
-#define EPS 1e-6
+#include "uniform_mesh_crushing.h"
+#define EPS 1e-5
 #define pi 3.141593
 
 void test(rr_func f, double a, double b, double expected_res, int expected_err_code, char *message) {
@@ -9,14 +9,20 @@ void test(rr_func f, double a, double b, double expected_res, int expected_err_c
     int err_code;
     res = uniform_mesh_crushing(f, a, b, EPS, &err_code);
 
-    if (err_code == expected_err_code || fabs(res - expected_res) < EPS) {
-        printf("Test passed!\n%s\n", message);
+    if (err_code == LACK_OF_ACCURACY && expected_err_code == LACK_OF_ACCURACY && fabs(res - expected_res) >= EPS) {
+	printf("%s\nTest passed!\n\n", message);
+        return ;
+    } 
+
+    else if (err_code == expected_err_code || fabs(res - expected_res) < EPS) {
+        printf("%s\nTest passed!\n\n", message);
         return ;
     }
 
     else {
         printf("expected result:                       %lf\nresult by uniform mesh crushing method:%lf\n", expected_res, res);
-        printf("%s\n******TEST FAILED!******\n", message);
+	printf("expected error code:%d\nerror code:         %d\n ", expected_err_code, err_code);
+	printf("%s\n******TEST FAILED!******\n\n", message);
     }
 }
 
@@ -46,7 +52,7 @@ double f6(double x) {
 }
 
 double f7(double x) {
-    return pow(x, 5) + 1e+5;
+    return pow(x, 5) + 1e+3;
 }
 
 double f8(double x) {
@@ -54,10 +60,18 @@ double f8(double x) {
 }
 
 double f9(double x) {
-    return 1/x - 1;
+    return 1/x;
 }
 
 int main() {
-
-                                                              45,1          35%
-
+    test(f1, 10., -20., -1, WRONG_SEGMENT, "Плохой отрезок");
+    test(f1, -10., 0., -10., LACK_OF_ACCURACY, "Прямая");
+    test(f1, -1e-4, 1e-7, -1e-4, OK_RES, "Прямая с маленьким отрезком");
+    test(f2, -3., 4., -1., OK_RES, "Парабола");
+    test(f3, -1., 2., -1.08866, OK_RES, "Неприятное значение");
+    test(f4, 2., 4., -1., OK_RES, "cos");
+    test(f5, -1e+3, 1e+4, -1., OK_RES, "Большой отрезок");
+    test(f7, -1., 1., 999., LACK_OF_ACCURACY, "Степенная функция");
+    test(f9, -100., -90., -1e-2, LACK_OF_ACCURACY, "Гипербола");
+    return 0;
+}
--- Min_value/uniform_mesh_crushing.c
+++ Min_value/uniform_mesh_crushing.c
@@ -1,52 +1,54 @@
 #include <math.h>
 #include "uniform_mesh_crushing.h"
 
-static double next_segment_search(rr_func f, double a, double b, double *a0, double *b0, double *x0) {
+static double next_segment_search(rr_func f, double eps, double a, double b, double *a0, double *b0, double *x0) {
+    double f_b = (*f)(b);
     double step = (b - a) / 100.0;
     double x1 = a;
-    double x3 = a + step;
-    double x2 = (x3 - x1) / 2.0;
-    double f_x1 = f(x1);
-    double f_x2 = f(x2);
-    double f_x3 = f(x3);
+    double x3 = x1 + step;
+    double x2 = (x3 + x1) / 2.0;
+    double f_x1 = (*f)(x1);
+    double f_x2 = (*f)(x2);
+    double f_x3 = (*f)(x3);
     double f_min = f_x2;
     
-    for (int i = 0; i < 100; i++) {
-	if ((f_x2 < f_x1) && (f_x2 < f_x3) && (f_x2 < f_min)) {
+    do {
+	if (((f_x2 <= f_x1) || (f_x2 <= f_x3)) && (f_x2 < f_min)) {
 	    *a0 = x1;
             *b0 = x3;
 	    *x0 = x2;
-	    f_min = f_x0;
+	    f_min = f_x2;
 	}
         x1 += step;
 	x2 += step;
 	x3 += step;
-	f_x1 = f(x1);
-        f_x2 = f(x2);
-	f_x3 = f(x3);
-    }
+	f_x1 = (*f)(x1);
+        f_x2 = (*f)(x2);
+	f_x3 = (*f)(x3);
+    } while (fabs(x3 - b) >= eps);
     return f_min;
 }
 
 double uniform_mesh_crushing(rr_func f, double a, double b, double eps, int *err_code) {
-    double f_a = f(a);
-    double f_b = f(b);
-    double x = (b - a) / 2.0;
-    double f_x = f(x);
+    double f_a = (*f)(a);
+    double f_b = (*f)(b);
+    double x;
+    double f_x;
     double res = f_x;
     int amount_of_it = 0;
 
     if (a >= b){
 	*err_code = WRONG_SEGMENT;
-	return res;
+	return -1;
     }
 
     do {
-	f_x = next_segment_search(f, a, b, &a, &b, &x);
+	f_x = next_segment_search(f, eps, a, b, &a, &b, &x);
 	amount_of_it += 100;
-	f_a = f(a);
-	f_b = f(b);
-    } while (fabs(f_x - f_a) >= eps && fabs(f_x - f_b) >= eps && fabs(a - b) >= eps && amount_of_it < MAX_IT); 
+	f_a = (*f)(a);
+	f_b = (*f)(b);
+	
+    } while (fabs(a - b) >= eps && amount_of_it < MAX_IT); 
     
     res = f_x;
 
@@ -55,7 +57,7 @@ double uniform_mesh_crushing(rr_func f, double a, double b, double eps, int *err
 	return res;
     }
 
-    if (fabs(a - b) < eps && res < f_a && res < f_b) {
+    if (fabs(a - b) < eps && f_x < f_a && f_x < f_b) {
 	*err_code = OK_RES;
 	return res;
     }
